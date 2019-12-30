b9a52e6fe8f6501291a32a82663aa6467f30a7a0
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-30 16:23:33 +0000

    useless rename issue need to resolve

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: []
   };
 
@@ -19,18 +19,18 @@ class Counter extends Component {
 
   getBadgeClass() {
     let classes = 'badge m-2 badge-'
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
     return classes
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value: value } = this.state;
+    return value === 0 ? 'Zero' : value;
   }
 
   handleIncrement = product => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 })
+    this.setState({ value: this.state.value + 1 })
   };
 
   render() {
