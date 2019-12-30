64ac6ebdab65fd4eb6237cfc486e1849a63d2d24
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-30 12:05:12 +0000

    increment button increases the number badge

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,11 @@ class Counter extends Component {
     tags: []
   };
 
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags</p>;
     return (
@@ -17,9 +22,26 @@ class Counter extends Component {
     );
   }
 
+  getBadgeClass() {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+
   render() {
     return (
       <div>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sml'
+        >
+          Increment
+        </button>
         {this.state.tags.length === 0 && "Please create a new tag."}
         {this.renderTags()}
       </div>
