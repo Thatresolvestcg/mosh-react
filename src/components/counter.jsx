4e9c28b45f418dfc85bcd4b87507244a8db3a64a
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-31 17:13:26 +0000

    global reset button included

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,43 +1,35 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value
-  };
-
-  getBadgeClass() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { value } = this.state;
-    return value === 0 ? "0" : value;
-  }
-
-  handleIncrement = product => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ product: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sml"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
         </button>
       </div>
     );
+  }
+
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "0" : value;
   }
 }
 
